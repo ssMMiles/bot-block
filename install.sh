f57104bc100eefd4da55217e009b1f0f0cc6eae5
ssMMiles <github@milesmoonlove.com>
2023-07-20 23:18:14 +0100

    add sudo everywhere

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 set -e
 
 cargo build --release
-cp target/release/bot-block /usr/local/bin
+sudo cp target/release/bot-block /usr/local/bin
 
-cp systemd/bot-block.service /etc/systemd/system/bot-block.service
-cp systemd/bot-block.timer /etc/systemd/system/bot-block.timer
+sudo cp systemd/bot-block.service /etc/systemd/system/bot-block.service
+sudo cp systemd/bot-block.timer /etc/systemd/system/bot-block.timer
 
-systemctl daemon-reload
-systemctl enable bot-block.timer
-systemctl start bot-block.timer
+sudo systemctl daemon-reload
+sudo systemctl enable bot-block.timer
+sudo systemctl start bot-block.timer
